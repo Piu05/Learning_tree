4d23c922f53388cc09bf576ef0b94bd59bd57d33
Priyanka Sharma <87617223+Piu05@users.noreply.github.com>
2024-02-15 14:34:25 +0530

    Error solved

--- learning-tree/src/pages/Home.jsx
+++ learning-tree/src/pages/Home.jsx
@@ -4,7 +4,6 @@ import logo from "../pics/treelogo.gif";
 import seed from "../pics/seed.png";
 import seedbox from "../pics/seedbox.jpg";
 import seedling from "../pics/seedling.png";
-import { Link } from "react-router-dom";
 
 function Home(){
     const[isOver,setIsOver]=useState(false);
@@ -35,9 +34,8 @@ function Home(){
             <div className="soil"></div>
             {isOver?
             (
-              <Link to="/Early">
             <button className="next-button"> Next </button>
-            </Link>):(<div/>)}
+            ):(<div/>)}
           </div>
         </div>
       </>
