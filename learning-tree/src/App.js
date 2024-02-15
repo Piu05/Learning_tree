58b8e9ef6e40ae7f73cf365e609cb1d3d3c4384a
Priyanka Sharma <87617223+Piu05@users.noreply.github.com>
2024-02-15 14:33:08 +0530

    Update App.js

--- learning-tree/src/App.js
+++ learning-tree/src/App.js
@@ -4,22 +4,12 @@ import Contact from "./pages/Contact";
 import Early from "./pages/Early";
 import Inter from "./pages/Inter";
 import Advanced from "./pages/Advanced";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <Router>
     <div className="App">
-    <Switch>
-      <Route exact path="/Home"><Home/></Route>
-      <Route exact path="/Early">
-      <Early/></Route>
-     <Route exact path="/Inter"><Inter/></Route>
-     <Route exact path="/Advanced"><Advanced/></Route>
-     <Route exact path="/Contact"><Contact/></Route>
-     </Switch>
+    <Early/>
     </div>
-    </Router>
   );
 }
 
